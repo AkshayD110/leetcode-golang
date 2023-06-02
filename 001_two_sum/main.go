0d7ef912acf567010f5b250ea199b6c654bfb9ed
AkshayD110 <akshayd110@gmail.com>
2023-06-02 08:16:35 +0530

    TwoNumbers-optimized

--- 001_two_sum/main.go
+++ 001_two_sum/main.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 	nums := []int{2, 3, 5, 6}
-	output := twoSum(nums, 9)
+	output := twoSumOpti(nums, 9)
 	fmt.Println(output)
 }
 
-//brute force method
+//brute force method - O(n2)
 func twoSum(nums []int, target int) []int {
 	//note that since we need the index, we are not using the range kind of for loop. -- "for i, num := range nums{}""
 	for i := 0; i < len(nums); i++ {
@@ -19,5 +19,25 @@ func twoSum(nums []int, target int) []int {
 			}
 		}
 	}
+	return []int{}
+}
+
+//optimized - O(n)
+func twoSumOpti(nums []int, target int) []int {
+	reversemap := make(map[int]int)
+	for i, num := range nums {
+		diffVal := target - num
+		val, ok := reversemap[num]
+		if ok {
+			return []int{i, val} //note how to return a slice directly without creating a variable.
+		}
+		// The above two lines can be written in a single line statement link below
+		// if val, ok := myMap["foo"]; ok {
+		// do something here
+		// }
+
+		reversemap[diffVal] = i
+	}
+
 	return []int{}
 }
